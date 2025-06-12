b49daefbf65808d180834711e453eaa51395739a
H2py <ahdosj@gmail.com>
2025-06-13 03:19:27 +0900

    Solve a lot of practice 3 of Chapter 3

--- knk/chapter3/practice.c
+++ knk/chapter3/practice.c
@@ -1,22 +1,39 @@
+/* 
+Write a program that asks the user to enter the numbers from 1 to 16 (in any order) and then
+displays the numbers in a 4 by 4 arrangement, followed by the sums of the rows, columns,
+and diagonals:
+
+Enter the numbers from 1 to 16 in any order:
+
+16 3 2 13 5 10 11 8 9 6 7 12 4 15 14 1
+
+16 3 2 13
+5 10 11 8
+9 6 7 12
+4 15 14 1
+
+Row sums: 34 34 34 34
+Column sums: 34 34 34
+Diagonal sums: 34 34
+
+If the row, column, and diagonal sums are all the same (as they are in this example), the
+numbers are said to form a magic square. 
+
+The magic square shown here appears in a 1514
+engraving by artist and mathematician Albrecht Dürer. (Note that the middle numbers in the
+last row give the date of the engraving.)
+*/
+
 #include <stdio.h>
 
 int main(void)
 {
-    int item, month, day, year;
-    float unit_price;
+    int first, mid, last;
 
-    printf("Enter item number: ");
-    scanf("%d", &item);
-
-    printf("Enter unit price: ");
-    scanf("%f", &unit_price);
+    printf("Enter phone number [(xxx) xxx-xxxx]: ");
+    scanf("(%d) %d-%d", &first, &mid, &last);
     
-    printf("Enter a date (mm/dd/yyyy): ");
-    scanf("%d/%d/%d", &month, &day, &year);
-
-    printf("Item\tUnit\tPurchase\n");
-    printf("\tPrice\tDate\n");
-    printf("%d\t$%6.2f\t\t%02d/%02d/%04d\n", item, unit_price, month, day, year);
-
+    printf("You entered %d.%d.%d",first,mid,last);
+    
     return 0;
 }
