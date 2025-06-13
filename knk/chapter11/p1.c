cc267b3c2dcc430b1b5682806a9a147284ae7232
H2py <ahdosj@gmail.com>
2025-06-13 19:00:48 +0900

    p1.c

---
+++ knk/chapter11/p1.c
@@ -0,0 +1,34 @@
+/*
+
+1. Modify Programming Project 7 from Chapter 2 so that it includes the following function:
+
+void pay_amount (int dollars, int *twenties, int *tens,
+int *fives, int *ones);
+The function determines the smallest number of $20, $10, $5, and $1 bills necessary to pay
+the amount represented by the do1lars parameter. The twenties parameter points to a
+variable in which the function will store the number of $20 bills required. The tens,.
+fives, and ones parameters are similar.
+
+
+1.
+Suppose that the following declarations are in effect:
+
+int a[] = {5, 15, 34, 54, 14, 2, 52, 72};
+int *p = &a [1], *q = &a [5];
+
+(a) What is the value of * (p+3)?
+(b) What is the value of * (q-3)?
+(c) What is the value of q - р?
+(d) Is the condition p < q true or false?
+(e) Is the condition *p < *q true or false?
+
+(A) 14 (B) 34 (C) 4 (D) TRUE (E) FALSE
+
+2. Suppose that high, low, and middle are all pointer variables of the same type, and that
+low and high point to elements of an array. Why is the following statement illegal, and
+how could it be fixed?
+
+middle = (low + high) / 2;
+
+
+*/
